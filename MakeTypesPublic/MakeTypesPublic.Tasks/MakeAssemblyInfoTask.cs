2110020eba24281260d57581e0a98f6070a2f5a4
Denis535 <dddenisss35@gmail.com>
2020-11-01 02:04:46 +0200

    Refactoring

---
+++ MakeTypesPublic/MakeTypesPublic.Tasks/MakeAssemblyInfoTask.cs
@@ -0,0 +1,53 @@
+﻿namespace MakeTypesPublic.Tasks {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
+    public class MakeAssemblyInfoTask : Task {
+
+        private const string Usage_IgnoresAccessChecksToAttribute =
+"[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo( \"{0}\" )]";
+
+        private const string Declaration_IgnoresAccessChecksToAttribute =
+@"namespace System.Runtime.CompilerServices {
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    internal sealed class IgnoresAccessChecksToAttribute : Attribute {
+        public IgnoresAccessChecksToAttribute(string assembly) {
+        }
+    }
+}";
+
+        [Required]
+        public string[] References { get; set; }
+        [Required]
+        public string DirectoryToSave { get; set; }
+
+
+        public override bool Execute() {
+            var builder = new StringBuilder();
+            foreach (var item in References.Select( Path.GetFileNameWithoutExtension )) {
+                builder.AppendFormat( Usage_IgnoresAccessChecksToAttribute, item ).AppendLine();
+            }
+            builder.AppendLine();
+            builder.AppendLine( Declaration_IgnoresAccessChecksToAttribute );
+
+            var path = Path.Combine( DirectoryToSave, "AssemblyInfo.cs" );
+            Save( path, builder.ToString(), Log );
+            return true;
+        }
+
+
+        // Helpers
+        private static void Save(string path, string content, TaskLoggingHelper log) {
+            Directory.CreateDirectory( Path.GetDirectoryName( path ) );
+            File.WriteAllText( path, content );
+            log.LogMessage( MessageImportance.High, "[MakeTypesPublic] Output: {0}", path );
+        }
+
+
+    }
+}
---
+++ MakeTypesPublic/MakeTypesPublic.Tasks/MakeAssemblyTask.cs
@@ -0,0 +1,44 @@
+﻿namespace MakeTypesPublic.Tasks {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+    using Mono.Cecil;
+
+    public class MakeAssemblyTask : Task {
+
+        [Required]
+        public string Reference { get; set; }
+        [Required]
+        public string DirectoryToSave { get; set; }
+
+        [Output]
+        public string OldReference { get; set; }
+        [Output]
+        public string NewReference { get; set; }
+
+
+        public override bool Execute() {
+            var assembly = AssemblyDefinition.ReadAssembly( Reference );
+            AssemblyWithPublicTypesMaker.MakeTypesPublic( assembly, Log );
+
+            var path = Path.Combine( DirectoryToSave, Path.GetFileName( Reference ) );
+            OldReference = Reference;
+            NewReference = path;
+            Save( path, assembly, Log );
+            return true;
+        }
+
+
+        // Helpers
+        private static void Save(string path, AssemblyDefinition assembly, TaskLoggingHelper log) {
+            Directory.CreateDirectory( Path.GetDirectoryName( path ) );
+            assembly.Write( path );
+            log.LogMessage( MessageImportance.High, "[MakeTypesPublic] Output: {0}", path );
+        }
+
+
+    }
+}
