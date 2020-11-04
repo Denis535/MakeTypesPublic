bb7400387b16c9fe827fee75472756fc68f9ab82
Denis535 <dddenisss35@gmail.com>
2020-11-05 00:41:09 +0200

    Refactoring

--- MakeTypesPublic/MakeTypesPublic.Tasks/MakeAssemblyWithPublicTypesTask.cs
+++ MakeTypesPublic/MakeTypesPublic.Tasks/MakeAssemblyWithPublicTypesTask.cs
@@ -1,7 +1,6 @@
 ﻿namespace MakeTypesPublic.Tasks {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.IO;
     using System.Text;
     using Microsoft.Build.Framework;
