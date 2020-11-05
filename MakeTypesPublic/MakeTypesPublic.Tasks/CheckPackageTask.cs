b3b2957abf5196168b83f95f4bf65254d9972051
Denis535 <dddenisss35@gmail.com>
2020-11-05 14:11:44 +0200

    CheckPackageTask

---
+++ MakeTypesPublic/MakeTypesPublic.Tasks/CheckPackageTask.cs
@@ -0,0 +1,29 @@
+﻿namespace MakeTypesPublic.Tasks {
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Text;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
+    public class CheckPackageTask : Task {
+
+        [Required]
+        public string MSBuildThisFileFullPath { get; set; }
+
+        private string AssemblyVersion => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+
+
+        public override bool Execute() {
+            if (!MSBuildThisFileFullPath.Contains( "\\" + AssemblyVersion + "\\" )) {
+                Log.LogError( "Package 'MakeTypesPublic' is loaded incorrectly" );
+                Log.LogMessage( MessageImportance.High, "[MakeTypesPublic] Error: Package is loaded incorrectly" );
+                Log.LogMessage( MessageImportance.High, "[MakeTypesPublic] MSBuildThisFileFullPath: {0}", MSBuildThisFileFullPath );
+                Log.LogMessage( MessageImportance.High, "[MakeTypesPublic] AssemblyVersion: {0}", AssemblyVersion );
+            }
+            return true;
+        }
+
+
+    }
+}
